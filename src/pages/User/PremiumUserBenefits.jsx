e897f4e3c8517127908bab1b9bfea83e3024176a
Sridhar Vellingiri <sridhar@provez.ai>
2025-02-27 12:27:43 +0530

    premium user benefits

--- src/pages/User/PremiumUserBenefits.jsx
+++ src/pages/User/PremiumUserBenefits.jsx
@@ -27,6 +27,66 @@ const PremiumUserBenefits = () => {
                                                 <div className="form-tit">
                                                     <h4>Premium User Benefits</h4>
                                                     <h2>Set your privacy benefits</h2>
+                                                    <div class="fol-sett-box">
+                                                        <ul>
+                                                            <li>
+                                                                <div class="sett-lef">
+                                                                    <div class="sett-rad-left">
+                                                                        <h5>Interest request</h5>
+                                                                        <p>Interest request email notificatios</p>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="sett-rig">
+                                                                    <div class="checkboxes-and-radios">
+                                                                        <input type="checkbox" name="checkbox-cats" id="sett-not-mail" value="1" checked="" />
+                                                                        <label for="sett-not-mail"></label>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                            <li>
+                                                                <div class="sett-lef">
+                                                                    <div class="sett-rad-left">
+                                                                        <h5>Chat</h5>
+                                                                        <p>New chat notificatios</p>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="sett-rig">
+                                                                    <div class="checkboxes-and-radios">
+                                                                        <input type="checkbox" name="checkbox-cats" id="sett-not-fri" value="1" checked="" />
+                                                                        <label for="sett-not-fri"></label>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                            <li>
+                                                                <div class="sett-lef">
+                                                                    <div class="sett-rad-left">
+                                                                        <h5>Profile views</h5>
+                                                                        <p>If any one view your profile means you get the notifications at end of the day</p>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="sett-rig">
+                                                                    <div class="checkboxes-and-radios">
+                                                                        <input type="checkbox" name="checkbox-cats" id="sett-not-fol" value="1" checked="" />
+                                                                        <label for="sett-not-fol"></label>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                            <li>
+                                                                <div class="sett-lef">
+                                                                    <div class="sett-rad-left">
+                                                                        <h5>New profile match</h5>
+                                                                        <p>You get the profile match emails</p>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="sett-rig">
+                                                                    <div class="checkboxes-and-radios">
+                                                                        <input type="checkbox" name="checkbox-cats" id="sett-not-mes" value="1" checked="" />
+                                                                        <label for="sett-not-mes"></label>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
                                                 </div>
                                             </div>
 
